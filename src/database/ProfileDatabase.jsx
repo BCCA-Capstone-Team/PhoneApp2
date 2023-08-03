6d55176df312ac66d5955aed4211fd4a42514c28
Capttech <31114186+Capttech@users.noreply.github.com>
2023-08-03 14:27:23 -0500

    Disable Debug For Profile Creation

--- src/database/ProfileDatabase.jsx
+++ src/database/ProfileDatabase.jsx
@@ -11,7 +11,7 @@ class ProfileDatabase extends Database {
 
     async startProfileSystem() {
         this.table = await this.createTable('profileDatabase', column => {
-            column.autoClear();
+            //column.autoClear();
 
             column.create('firstName', 'TEXT');
             column.create('lastName', 'TEXT');
